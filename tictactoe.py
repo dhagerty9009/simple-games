026b26af68191a08960587a2e7c3ca708153da63
David Hagerty <hagerty9009@gmail.com>
2010-12-13 13:50:09 -0500

    fixed tic-tac-toe issue

--- tictactoe.py
+++ tictactoe.py
@@ -68,10 +68,10 @@ def chooseRandomMoveFromList(board,movesList):
 	for i in movesList:
 		if isSpaceFree(board,i):
 			possibleMoves.append(i)
-		if len(possibleMoves) != 0:
-			return random.choice(possibleMoves)
-		else:
-			return None
+	if len(possibleMoves) != 0:
+		return random.choice(possibleMoves)
+	else:
+		return None
 def getComputerMove(board,computerLetter):
 	#given a board and the computer's letter, find the optimal move
 	if computerLetter=="X":
