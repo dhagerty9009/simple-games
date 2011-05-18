feab603e81381da29771e5b644254ae2a91a71af
David Hagerty <hagerty9009@gmail.com>
2011-05-18 15:01:20 -0400

    Added: checkers game file Edited: checkers game file - added in function to get the player's tile choice

---
+++ checkers.rb
@@ -0,0 +1,30 @@
+# simple-games.checkers, written by David Hagerty
+# Copyright 2011, licenced under the GPL
+#
+# The goal of checkers is to eliminate all of your opponent's pieces
+# You can only move diagonally forward, unless the piece is a king.
+# Player pieces will be denoted by the letter "P", kings by "PK"
+# Computer pieces will be denoted by "C", kings by "CK"
+
+def getPlayerColor()
+  player_choice = ""
+  while player_choice != "B" or player_choice != "W"
+    puts "What color do you want to be?"
+    puts "Black (B)? Or White (W)?"
+    player_choice = gets.chomp.capitalize
+    if player_choice == "B"
+      computer_choice = "W"
+      break
+    else player_choice == "W"
+      computer_choice = "B"
+      break
+    end
+  end
+  return player_choice, computer_choice
+end
+
+puts "Welcome to the game of checkers!"
+puts "Please choose your color."
+player_color, computer_color = getPlayerColor()
+puts "Your pieces are marked by "+player_color+"s."
+puts "The computer's pieces are marked by "+computer_color+"s"
